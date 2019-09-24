01a19987ee9a299437fc6e99227eba16fb2b9cde
odooa <info@appalach.be>
2019-09-24 19:16:10 +0200

    ajout de joueur et joueurDAO

---
+++ classesMetier/Joueurs.java
@@ -0,0 +1,65 @@
+package classesMetier;
+
+import java.sql.PreparedStatement;
+
+public class Joueurs
+{
+    private int Idj;
+    private String Nom;
+    private String Prenom;
+    private String Style;
+    private String NomImage;
+    /**
+     * propriété get... pour obtenir la valeur des attributs
+     */
+    public int getIda() { return Idj; }
+
+    public String getNom() { return Nom; }
+
+    public String getPrenom() { return Prenom; }
+
+    public String getStyle() {return Style; }
+
+    public String getNomImage() { return NomImage; }
+
+    /**
+     * propriété set... pour modifier la valeur des attributs
+     */
+
+    public void setIda(int idj) { Idj = idj; }
+
+    public void setNom(String nom) { Nom = nom; }
+
+    public void setPrenom(String prenom) { Prenom = prenom; }
+
+    public void setStyle(String style) {Style = style;}
+
+    public void setNomImage(String nom) { NomImage = nom; }
+
+    /**
+     * constructeurs
+     */
+    public Joueurs()
+    {
+    }
+    public Joueurs(Joueurs jou)
+    {
+        Idj = jou.Idj;
+        Nom = jou.Nom;
+        Prenom = jou.Prenom;
+        Style = jou.Style;
+        NomImage = jou.NomImage;
+
+    }
+    public Joueurs(int idj, String nom,
+                    String prenom, String style, String nomImage)
+    {
+        Idj = idj;
+        Nom = nom;
+        Prenom = prenom;
+        Style = style;
+        NomImage = nomImage;
+    }
+}
+
+
---
+++ coucheAccesBD/JoueursDAO.java
@@ -0,0 +1,104 @@
+package coucheAccesBD;
+import java.sql.*;
+import java.util.*;
+import classesMetier.*;
+
+public class JoueursDAO extends BaseDAO<Joueurs>
+{
+    public JoueursDAO(Connection sqlConn)
+    {
+        super(sqlConn);
+    }
+
+    /**
+     * méthode qui lit dans la base de données tous les cours
+     * @return la liste de tous les cours
+     */
+
+    public List<Joueurs> listerTous() throws ExceptionAccesBD
+    {
+        ArrayList<Joueurs> liste = new ArrayList<Joueurs>();
+        try
+        {
+            SqlCmd = SqlConn.prepareCall("select idj, nom, prenom, style, nomImage " +
+                    "from joueurs " +
+                    "order by nom asc");
+
+            ResultSet sqlRes = SqlCmd.executeQuery();
+
+            while (sqlRes.next() == true)
+                liste.add(new Joueurs(sqlRes.getInt(1),
+                        sqlRes.getString(2),
+                        sqlRes.getString(3),
+                        sqlRes.getString(4),
+                        sqlRes.getString(5)));
+            sqlRes.close();
+        }
+        catch(Exception e)
+        {
+            throw new ExceptionAccesBD(e.getMessage());
+        }
+
+        return liste;
+    }
+
+    /**
+     * méthode qui ajoute dans la base de données un joueur
+     * @param obj : le joueur
+     */
+
+    public void ajouter(Joueurs obj) throws ExceptionAccesBD
+    {
+        try
+        {
+            SqlCmd = SqlConn.prepareCall("select max(idj) + 1 from joueurs");
+
+            ResultSet sqlRes = SqlCmd.executeQuery();
+            sqlRes.next();
+
+            int idj = sqlRes.getInt(1);
+            if(sqlRes.wasNull()) idj = 1;
+
+            SqlCmd.close();
+
+            SqlCmd = SqlConn.prepareCall("insert into joueurs values(?, ?, ?, ?, ?)");
+
+            SqlCmd.setInt(1, idj);
+            SqlCmd.setString(2, obj.getNom());
+            SqlCmd.setString(3, obj.getPrenom());
+            SqlCmd.setString(4, obj.getStyle());
+            SqlCmd.setString(5, obj.getNomImage());
+
+            SqlCmd.executeUpdate();
+        }
+        catch(Exception e)
+        {
+            throw new ExceptionAccesBD(e.getMessage());
+        }
+    }
+
+    /**
+     * méthode qui supprime dans la base de données un joueur
+     * @param num : le numéro du joueur
+     */
+
+    public void supprimer(int num) throws ExceptionAccesBD
+    {
+        try
+        {
+            SqlConn.setAutoCommit(false);
+
+            SqlCmd = SqlConn.prepareCall("delete from eleve where idj = ?");
+            SqlCmd.setInt(1, num);
+
+            SqlCmd.executeUpdate();
+        }
+        catch(Exception e)
+        {
+            throw new ExceptionAccesBD(e.getMessage());
+        }
+    }
+
+
+
+}
